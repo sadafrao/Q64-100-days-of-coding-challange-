ade937a4d4144e72b7efa4d1a799c8620532f97c
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:14:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q64
+// this function make a text and num into one text
+function makeTextAndNum(text, num) {
+    return text + num;
+}
+console.log(makeTextAndNum("age:", 30));
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q64
+// this function make a text and num into one text
+function makeTextAndNum(text: string, num: number): string {
+    return text + num;
+}
+console.log(makeTextAndNum("age:", 30));
